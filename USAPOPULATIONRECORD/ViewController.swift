e84bc281ebf3c3319f1b47bba6022387dabb17ed
vikram <vikramkunwar1404@gmail.com>
2023-04-17 17:51:02 +0530

    Initial Commit

---
+++ USAPOPULATIONRECORD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  USAPOPULATIONRECORD
+//
+//  Created by Vikram Kunwar on 17/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
